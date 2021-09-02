36494541ea2c95e3137fbe83314cb5deb8071da2
Muhayminul Nirnoy <86642381+Nirnoy0@users.noreply.github.com>
2021-09-02 17:41:58 +0600

    show data

--- JS/main.js
+++ JS/main.js
@@ -2,32 +2,34 @@ const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value= '';
-
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-
     fetch(url)
         .then(res => res.json())
-        .then(data => displaySearchResult(data.docs[0]));
+        .then(data => displaySearchResult(data.docs));
 }
 
-
 // Display search result
-
-const displaySearchResult = books => {
-    const searchResult = document.getElementById('search-result')
-    books.docs[0].forEach(book => {
+const displaySearchResult = docs => {
+    const searchResult = document.getElementById('search-result');
+    docs.forEach(doc => {
+        // console.log(doc.title);
+        // console.log(doc.author_name);
+        // console.log(doc.first_publish_year);
+        // console.log(doc.publisher);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card">
-                <img src="https://covers.openlibrary.org/b/id=${books.cover_i}-M.jpg" class="card-img-top">
+                <img src="https://covers.openlibrary.org/b/id/=${doc.cover_i}-M.jpg" class="card-img-top" alt="Books Images">
                 <div class="card-body">
-                    <h3 class="card-title">${book.docs[0].title}</h3>
-                    <p class="card-text">${book.docs[0].author_name}</p>
-                    <p class="card-text">${book.docs[0].first_publish_year}</p>
-                    <p class="card-text">${book.docs[0].publisher}</p>    
+                    <h5 class="card-title">${doc.title}</h5>
+                    <p class="card-text">
+                    Author: ${doc.author_name}<br>
+                    Publish Year: ${doc.first_publish_year}<br>
+                    Publisher: ${doc.publisher}
+                    </p>
                 </div>
-            <div>
+            </div>
         `;
         searchResult.appendChild(div);
     });
