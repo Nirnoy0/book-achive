30458a0d3eeb7a813b61c68bdb6fa09752ab4597
Muhayminul Nirnoy <86642381+Nirnoy0@users.noreply.github.com>
2021-09-02 23:38:57 +0600

    final

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,40 @@
+//  spinner
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
+
+
+// search book
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    toggleSpinner('block');
     searchField.value= '';
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.docs));
-}
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.docs)); 
+
+    const totalDataFound = document.getElementById('total-data');
+    totalDataFound.innerHTML = '';
+    const div = document.createElement('div');
+    div.classList.add('data');
+    div.innerHTML = `
+        <div>
+         Total Data Found: ${url.length}
+        </div>
+    `;
+    totalDataFound.appendChild(div);
+} 
+
+
 
 // Display search result
 const displaySearchResult = docs => {
     const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = '';
+
     docs.forEach(doc => {
         const div = document.createElement('div');
         div.classList.add('col');
@@ -29,6 +53,7 @@ const displaySearchResult = docs => {
         `;
         searchResult.appendChild(div);
     });
+    toggleSpinner('none');
 }
 
 
